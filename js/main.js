18e7f19f190fcd3bd65d579d451198010acd332a
Brett Adamson <adamsonb12@gmail.com>
2016-10-02 19:06:37 -0600

    First Commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//Custom Javascript
+
+function googleModal() {//function to bring up the modal for GFE
+	alert("Google Educaiton is the shit");
+}
+
---
+++ js/masonry.js
@@ -0,0 +1,22 @@
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+    gutter: 10
+});
+
+// init Masonry
+var $grid = $('.grid').masonry({
+  // options...
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
